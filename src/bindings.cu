6910600805f87b780bfefdee546b0e1f1971965f
dq2024 <dq2024@nyu.edu>
2025-12-03 14:47:00 -0500

    first commit

--- src/bindings.cu
+++ src/bindings.cu
@@ -1,6 +1,25 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include "llama_bindings.hpp"
+#include "ops/op_elemwise.cuh"
+
+// Define the extern variable
+unsigned long long randgen_seed = 42;
+
+// ... rest of your code ...
+
+PYBIND11_MODULE(bten, m) {
+    // Add this line
+    m.def("randgen_seed", [](unsigned long long seed) {
+        randgen_seed = seed;
+    }, "Set random seed");
+    
+    // ... rest of your existing bindings ...
+    bind_tensor_type<float>(m, "Tensor");
+    bind_tensor_type<uint32_t>(m, "TensorU32");
+    bind_llama_model(m);
+}
+
 
 namespace py = pybind11;
 
