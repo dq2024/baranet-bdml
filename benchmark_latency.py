fd8a9790c15b10d72709a95130220bba1e1726d3
dq2024 <dq2024@nyu.edu>
2025-12-11 14:28:40 -0500

    first commit

--- benchmark_latency.py
+++ benchmark_latency.py
@@ -29,12 +29,14 @@ def benchmark_python_impl(batch_size: int, num_warmup: int = 5, num_iterations: 
     
     # Create model
     print("Creating model...")
-    from mygrad.engine import no_grad
+    config = LLaMAConfig()
+    config.vocab_size = 1000  # Smaller for faster testing
+    config.hidden_size = 256
+    config.num_hidden_layers = 4
+    config.num_attention_heads = 8
+    config.num_key_value_heads = 2
     
-    # Create full TinyLLaMA model
-    print("Creating model...")
     model = create_tinyllama_model(is_cuda=True)
-    config = model.config
     
     # Create input
     input_ids = np.random.randint(0, config.vocab_size, size=batch_size, dtype=np.uint32)
